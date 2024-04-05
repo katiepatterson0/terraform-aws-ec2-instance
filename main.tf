06d0eb8e2ab5ba3e51efb8b2310ffaf516c558af
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-05 13:33:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "my_other_app_server" {
+resource "aws_instance" "my_third_app_server" {
   ami           = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Example EC2 for demo from CLI"
+    Name = "Example EC2 for demo from PMR"
   }
 }
