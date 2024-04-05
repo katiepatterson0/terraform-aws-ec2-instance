666d6c69847d1ff63a20029e34e6bbe86593ddac
katiepatterson0 <82819228+katiepatterson0@users.noreply.github.com>
2024-04-05 13:32:59 -0400

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.0.1, <5.43.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws-access-key
+  secret_key = var.aws-secret-access-key
+}
+
+terraform {
+  cloud {
+    organization = "patterson-demo"
+
+    workspaces {
+      name = "tfc_democli"
+    }
+  }
+}
